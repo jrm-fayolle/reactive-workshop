bf830b6908fb3a76ee7c0cd78e031cfdedf4c71c
Jérôme FAYOLLE <jerome.fayolle@comarch.com>
2021-04-20 11:09:44 +0200

    fix docker issues

--- backend/src/main/java/com/bonitasoft/reactiveworkshop/Constants.java
+++ backend/src/main/java/com/bonitasoft/reactiveworkshop/Constants.java
@@ -2,7 +2,7 @@ package com.bonitasoft.reactiveworkshop;
 
 public class Constants {
 
-	public static String COMMENT_URI = "http://localhost:3004";
+	public static String COMMENT_URI = System.getProperty("external-service","http://localhost:3004");
 	public static String COMMENTS_ARTIST_LAST10 = "/comments/{id}/last10";
 	public static String COMMENTS_STREAM = "/comments/stream";	
 
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call gradlew :backend:bootJar
 call gradlew :external-service:bootJar
 
+call docker-compose down
 call docker build -t backend-img .
-
-call docker compose up
+call docker-compose up -d
